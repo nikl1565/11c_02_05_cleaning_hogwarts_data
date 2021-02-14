cafdec401f12044ab333a4659ba4e86be62859c5
Niklas Vejlby Schjoldager <nikl1565@stud.kea.dk>
2021-02-14 10:47:47 +0100

    All images now displaying :-)

--- js/script.js
+++ js/script.js
@@ -13,6 +13,8 @@ const Student = {
     image: undefined,
 };
 
+let unfilteredStudents;
+
 function init() {
     console.log("init");
 
@@ -27,6 +29,9 @@ function loadStudents() {
     fetch("https://petlatkea.dk/2021/hogwarts/students.json")
         .then((response) => response.json())
         .then((jsonData) => {
+            unfilteredStudents = jsonData;
+            console.log("unfilteredStudents", unfilteredStudents);
+
             prepareObjects(jsonData);
         });
 }
@@ -43,16 +48,19 @@ function prepareObjects(jsonData) {
         const lastSpaceInFullName = fullName.lastIndexOf(" ");
         const checkForHyphen = fullName.indexOf("-");
 
+        let firstName;
         let firstNameFirstLetter;
         // Firstname
         if (firstSpaceInFullName === -1) {
             firstNameFirstLetter = fullName[0].toUpperCase();
             const firstNameAfterFirstLetter = fullName.substring(1);
+            firstName = firstNameFirstLetter + firstNameAfterFirstLetter;
             student.firstName = firstNameFirstLetter + firstNameAfterFirstLetter;
         } else {
             const findFirstName = fullName.substring(0, firstSpaceInFullName);
             firstNameFirstLetter = findFirstName[0].toUpperCase();
             const firstNameAfterFirstLetter = findFirstName.substring(1);
+            firstName = firstNameFirstLetter + firstNameAfterFirstLetter;
             student.firstName = firstNameFirstLetter + firstNameAfterFirstLetter;
         }
 
@@ -82,23 +90,29 @@ function prepareObjects(jsonData) {
 
         // Lastname
         const lastName = fullName.substring(lastSpaceInFullName).trim();
-        const hasLastNameHyphen = lastName.indexOf("-");
-        // If no hyphen
-        if (hasLastNameHyphen === -1) {
-            const lastNameFirstLetter = lastName[0].toUpperCase();
-            const lastNameAfterFirstLetter = lastName.substring(1);
-            student.lastName = lastNameFirstLetter + lastNameAfterFirstLetter;
-            // Else has hyphen
-        } else {
-            const lastNameFirstLetter = lastName[0].toUpperCase();
-            const beforeHyphenUppercase = lastName.substring(1, hasLastNameHyphen + 1);
-            console.log("beforeHyphen", beforeHyphenUppercase);
-            const hyphenUppercase = lastName[hasLastNameHyphen + 1].toUpperCase();
-            console.log("hyphenUppercase", hyphenUppercase);
-            const afterHyphenUppercase = lastName.substring(hasLastNameHyphen + 2);
-            console.log("afterHyphenUppercase", afterHyphenUppercase);
+        let hasLastNameHyphen;
+        let afterHyphenUppercase;
 
-            student.lastName = lastNameFirstLetter + beforeHyphenUppercase + hyphenUppercase + afterHyphenUppercase;
+        if (firstName.toLowerCase() !== lastName.toLowerCase()) {
+            console.log(lastName);
+            hasLastNameHyphen = lastName.indexOf("-");
+            // If no hyphen
+            if (hasLastNameHyphen === -1) {
+                const lastNameFirstLetter = lastName[0].toUpperCase();
+                const lastNameAfterFirstLetter = lastName.substring(1);
+                student.lastName = lastNameFirstLetter + lastNameAfterFirstLetter;
+                // Else has hyphen
+            } else {
+                const lastNameFirstLetter = lastName[0].toUpperCase();
+                const beforeHyphenUppercase = lastName.substring(1, hasLastNameHyphen + 1);
+                console.log("beforeHyphen", beforeHyphenUppercase);
+                const hyphenUppercase = lastName[hasLastNameHyphen + 1].toUpperCase();
+                console.log("hyphenUppercase", hyphenUppercase);
+                afterHyphenUppercase = lastName.substring(hasLastNameHyphen + 2);
+                console.log("afterHyphenUppercase", afterHyphenUppercase);
+
+                student.lastName = lastNameFirstLetter + beforeHyphenUppercase + hyphenUppercase + afterHyphenUppercase;
+            }
         }
 
         // House
@@ -113,9 +127,38 @@ function prepareObjects(jsonData) {
         const genderAfterFirstLetter = gender.substring(1);
         student.gender = genderFirstLetter + genderAfterFirstLetter;
 
-        // Image
-        student.image = `img/${lastName}_${firstNameFirstLetter.toLowerCase()}.png`;
+        console.log(student.lastName);
 
+        // Image
+        let multipleWithTheSameLastName = 0;
+
+        unfilteredStudents.forEach((unfilteredStudent) => {
+            const lastSpace = unfilteredStudent.fullname.lastIndexOf(" ");
+            const lastName = unfilteredStudent.fullname
+                .substring(lastSpace + 1)
+                .trim()
+                .toLowerCase();
+            console.log("lol", lastName);
+
+            if (lastName === student.lastName.toLowerCase()) {
+                multipleWithTheSameLastName++;
+            }
+        });
+
+        // If no lastname
+        if (firstName.toLowerCase() === lastName.toLowerCase()) {
+            student.image = `https://images.unsplash.com/photo-1598153346810-860daa814c4b?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1780&q=80`;
+            // Else if lastname with hyphen
+        } else if (hasLastNameHyphen !== -1) {
+            student.image = `img/${lastName.substring(hasLastNameHyphen + 1)}_${firstNameFirstLetter.toLowerCase()}.png`;
+            // Else normal way
+        } else if (multipleWithTheSameLastName >= 2) {
+            console.log("lol");
+            console.log("firstName", firstName);
+            student.image = `img/${lastName}_${firstName}.png`;
+        } else {
+            student.image = `img/${lastName}_${firstNameFirstLetter.toLowerCase()}.png`;
+        }
         // Add student to allStudent list
         allStudents.push(student);
     });
